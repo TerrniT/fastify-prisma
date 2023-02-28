d9c7884bfe8b7e8f44799e48f5d594c289469970
TerrniT <terrnit.megaman@gmail.com>
2023-02-28 16:42:05 +0500

    feat(init): bootstrap fastify server

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import Fastify from "fastify";
+
+const server = Fastify();
+
+server.get("/healthcheck", async function () {
+  return {
+    status: "OK",
+  };
+});
+
+async function main() {
+  try {
+    await server.listen(3000, "0.0.0.0");
+    console.log("Server ready at http://localhost:3000");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+main();
